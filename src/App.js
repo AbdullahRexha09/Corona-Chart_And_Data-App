8d6f6b7631934ec8ff223efcc9fe1e6c8dee6d24
Abdullah Rexha <Abdullah__rexha@hotmail.com>
2021-07-18 19:04:53 +0200

    Second

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import {Cards, Chart, CountryPicker} from './components'
+import styles from './App.module.css';
+import {fetchData} from './api'
+
+class App extends React.Component{
+    state = {
+        data:{},
+        country:'',
+    }
+
+    async componentDidMount(){
+        const data = await fetchData();
+
+        this.setState({ data });
+    }
+    handleCountryChange = async(country) => {
+        const data = await fetchData(country);
+        this.setState({data, country});
+        console.log(country);
+    }
+    render(){
+        const {data} = this.state;
+        return (
+            <div className={styles.container}>
+                <Cards data={data}/>
+                <CountryPicker handleCountryChange={this.handleCountryChange} />
+                <Chart />
+            </div>
+        );
+    }
+}
+export default App;
